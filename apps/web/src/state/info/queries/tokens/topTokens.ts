af3d8d3a5947baff79040cea962fadc2d2e46e69
Rahul AR <rahular@accubits.com>
2024-12-13 16:56:58 +0530

    denergy support: fix error on build

--- apps/web/src/state/info/queries/tokens/topTokens.ts
+++ apps/web/src/state/info/queries/tokens/topTokens.ts
@@ -28,7 +28,7 @@ interface StableSwapTopTokensResponse {
  */
 const fetchTopTokens = async (chainName: MultiChainNameExtend, timestamp24hAgo: number): Promise<string[]> => {
   const whereCondition =
-    chainName === 'ETH'
+    chainName === 'DENERGY'
       ? `where: { date_gt: ${timestamp24hAgo}, token_not_in: $blacklist, dailyVolumeUSD_gt:2000 }`
       : checkIsStableSwap()
       ? ''
